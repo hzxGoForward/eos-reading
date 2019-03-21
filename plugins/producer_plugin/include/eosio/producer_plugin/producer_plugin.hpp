27a8fd28cb411529cee9fb1b5456902c9299c8ea
hzx <hzx@pku.edu.cn>
2019-03-21 20:32:56 +0800

    Update producer_plugin.hpp

--- plugins/producer_plugin/include/eosio/producer_plugin/producer_plugin.hpp
+++ plugins/producer_plugin/include/eosio/producer_plugin/producer_plugin.hpp
@@ -59,32 +59,53 @@ public:
       boost::program_options::options_description &config_file_options
       ) override;
 
-   bool                   is_producer_key(const chain::public_key_type& key) const;
+
+   // 判断一个公钥是否是当前区块生产者的公钥
+   bool                   is_producer_key(const chain::public_key_type& key) const; 
+   // 应该是对摘要进行签名，这里面的key应该是私钥。        
    chain::signature_type  sign_compact(const chain::public_key_type& key, const fc::sha256& digest) const;
 
+   // 对插件进行初始化
    virtual void plugin_initialize(const boost::program_options::variables_map& options);
+   // 插件启动函数
    virtual void plugin_startup();
+   // 插件关闭函数
    virtual void plugin_shutdown();
-
+   // 暂停运行和继续运行
    void pause();
    void resume();
+   // 返回插件是否被暂停的状态
    bool paused() const;
+
+   // 更新运行的相关参数
    void update_runtime_options(const runtime_options& options);
+
+   // 获取运行时的相关参数
    runtime_options get_runtime_options() const;
 
+   // 增加grey_list
    void add_greylist_accounts(const greylist_params& params);
+   // 删除grey_list
    void remove_greylist_accounts(const greylist_params& params);
+   // 获取greylist
    greylist_params get_greylist() const;
 
+   // 获取whitelist和设置whitelist
    whitelist_blacklist get_whitelist_blacklist() const;
    void set_whitelist_blacklist(const whitelist_blacklist& params);
 
+   // 获取integrity_hash_information
    integrity_hash_information get_integrity_hash() const;
+   
+   // 创建一个快照，snapshot中文意思是快照
    snapshot_information create_snapshot() const;
 
+   // 定义一个返回值为空，传入变量为const chain::producer_confirmation&的signal
+   // 这个信号用于区块的确认。
    signal<void(const chain::producer_confirmation&)> confirmed_block;
 private:
-   std::shared_ptr<class producer_plugin_impl> my; // producer_plugin_impl的指针,负责所有任务的执行
+    // producer_plugin_impl的指针,负责所有任务的执行
+   std::shared_ptr<class producer_plugin_impl> my; 
 };
 
 } //eosio
@@ -94,4 +115,3 @@ FC_REFLECT(eosio::producer_plugin::greylist_params, (accounts));
 FC_REFLECT(eosio::producer_plugin::whitelist_blacklist, (actor_whitelist)(actor_blacklist)(contract_whitelist)(contract_blacklist)(action_blacklist)(key_blacklist) )
 FC_REFLECT(eosio::producer_plugin::integrity_hash_information, (head_block_id)(integrity_hash))
 FC_REFLECT(eosio::producer_plugin::snapshot_information, (head_block_id)(snapshot_name))
-
